ffd41dd38beedbabc4791bd5a10500914176551e
genilsonjrGN <genilson.junior@gentilnegocios.com.br>
2025-05-31 17:32:48 -0300

    autencicaaaaa

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.secret_key = os.getenv('FLASK_SECRET_KEY', 'sua_chave_secreta_aqui')
 
 # Configurações do Xano
 XANO_BASE_URL = os.getenv('XANO_BASE_URL', "https://xidg-u2cu-sa8e.n7c.xano.io/api:loOqZbWF")
-XANO_API_KEY = os.getenv('XANO_API_KEY', "eyJhbGciOiJBMjU2S1ciLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiemlwIjoiREVGIn0.kSdWT7G9CmEmNaOhKpQZZfb28JMCFZBTmDo3YGJVs8YAkGhw4Iu39-ScrGOuqz5_3F0S-1hLF7g2dYrkf9Pwlq6SQc3MA7jh.TmG-_PLZ4Yilefni9TnqIQ.08O-H7NHBcUFdb9KKHlh1Mj0Z8lhzeOpmR-YiAdHg-6for9QeXWD1b5D-lk5dCKBb_y1ffyTEfShc3_TE9Yxeh9Df2zDYecJeN3GQcXLwzEUVeCrnjCG0rK6fQiFqJf_uUKsT0kSShfLThVeMThWsw.Ueg0suuJJE02uxJg44SfUvWmFFqc0YssTS5u3jihSgo")
+XANO_API_KEY = os.getenv('XANO_API_KEY', "eyJhbGciOiJBMjU2S1ciLCJlbmMiOiJBMjU2Q0JDLUhTNTEyIiwiemlwIjoiREVGIn0.6TaIGBdXOiHz7tcy5yIEtjhek_Tx6X4BVupvGM7sm8Z3mT1gZ6Qzq8CyQRNdXPfEJjW0pee75p_eoqZ2JphenQHx60Ufs1LH.nBaN0foM8TrTRc7RQtV9RQ.5hFNA-Alp1Mf7raKlix90PYnXa8zm8W80hocZSdgWgjBF-Ci6tr-a4gDhufOeBvpLc7w_oXUQnC53gbqB8ReMvjwZJASMak0RbIsU1tsFmEuIg3F_VAyhKi-Pv0fiyBce3UWnovsdXzkVK1_22yh8Q.gTymCdWYorrIxN0bv_J9fIXnhlkrnNmQFfsvtD47HN8")
 # Estrutura das tabelas no Xano
 XANO_TABLES = {
     'users': 'user_eleva',
